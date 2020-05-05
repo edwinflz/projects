05b47727c86a5c84d0cd3ad281ecc5f8b0093a42
edwin95florez <edwin.95.florez@gmail.com>
2020-05-05 17:04:30 -0500

    Add: Project SPA - Router Javascript

---
+++ src/router-javascript/router.js
@@ -0,0 +1,43 @@
+class Router {
+
+    constructor(routes) {
+        this.routes = routes;
+        this._loadInitialRoute();
+    }
+
+    loadRoute(...urlSegm) {
+        let matchedRoute = this._mathUrlToRoute(urlSegm);
+
+        
+        // console.log(matchedRoute);
+
+        const url = `/${urlSegm.join('/')}`;
+        history.pushState({}, 'this works', url);
+
+        const routerOutElm = document.querySelectorAll('[data-router]')[0];
+        routerOutElm.innerHTML = matchedRoute.template;
+    }
+
+    _mathUrlToRoute(urlSegm) {
+        const matchedRoute = this.routes.find(route => {
+            const routePathSegs = route.path.split('/').slice(1);
+
+            if (routePathSegs.length !== urlSegm.length) {
+                return false;
+            }
+
+            return routePathSegs.every((routePathSeg, i) => routePathSeg === urlSegm[i]);
+
+        });
+
+        return matchedRoute;
+
+    }
+
+    _loadInitialRoute() {
+        const pathNameSplit = window.location.pathname.split('/');
+        const pathSegs = pathNameSplit.length > 1 ? pathNameSplit.slice(3) : '';
+
+        this.loadRoute(...pathSegs);
+    }
+}
