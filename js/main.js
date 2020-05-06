609badaac22af611a7269744de64e331b8cb2e91
edwin95florez <edwin.95.florez@gmail.com>
2020-05-05 19:38:33 -0500

    Feat: Animation Load image card

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
 $(function () {
     $('[data-toggle="tooltip"]').tooltip();
+    animationImg();
 });
+
+
+const animationImg = () => {
+    const cards = document.querySelectorAll('.card');
+
+    cards.forEach((card) => {
+        const image = card.querySelector('img');
+        image.addEventListener('load', (event) => {
+            event.srcElement.classList.add('fadeIn');
+        })
+    })
+
+
+}
