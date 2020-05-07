f28bc1754336b1a66fd2556ce182ae7a0d8dca04
edwin95florez <edwin.95.florez@gmail.com>
2020-05-07 15:44:08 -0500

    Feat: Project Main Menu Finished

---
+++ src/restaurante-bootstrap/js/main.js
@@ -0,0 +1,48 @@
+$(function () {
+
+    let sticky = false;
+
+    $stickyNavigation.removeClass('d-none');
+    $stickyNavigation.slideUp(0);
+
+
+    $(window).scroll(() => {
+        const inBottom = heightDescription();
+
+        if (inBottom && !sticky) {
+            sticky = true;
+            stickyNavigation();
+        }
+        if (!inBottom && sticky) {
+            sticky = false;
+            unStickyNavigation();
+        }
+    });
+    
+ 
+});
+
+const $stickyNavigation = $('#sticky-navigation');
+const $description = $('#description');
+const $navigation = $('#navigation');
+
+const stickyNavigation = () => {
+    $description.addClass('position-fixed').removeClass('position-absolute');
+    $navigation.slideUp('fast');
+    $stickyNavigation.slideDown('fast');
+}
+
+const unStickyNavigation = () => {
+    $description.removeClass('position-fixed').addClass('position-absolute');
+    $navigation.slideDown('fast');
+    $stickyNavigation.slideUp('fast');
+}
+
+const heightDescription = () => {
+    const $descriptionHeight = $description.height();
+    return scrollOnDescription($descriptionHeight);
+}
+
+const scrollOnDescription = (height) => {
+    return $(window).scrollTop() > $(window).height() - (height * 2);
+}
