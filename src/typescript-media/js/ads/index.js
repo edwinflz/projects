e45717bc6aebb72256f95e7e5cef9c22a3840d0e
edwin95florez <edwin.95.florez@gmail.com>
2020-07-09 16:54:07 -0500

    CV añadido V1

--- src/typescript-media/js/ads/index.js
+++ src/typescript-media/js/ads/index.js
@@ -1,43 +1,34 @@
 import Ads from './Ads.js';
 
 class AdsPlugin {
+  constructor() {
+    this.ads = Ads.getInstance();
+    this.adsContainer = document.createElement('div');
+    this._handleTimeUpdate = this._handleTimeUpdate.bind(this);
+  }
 
+  run(player) {
+    this.player = player;
+    this.player.container.appendChild(this.adsContainer);
+    this.media = this.player.media;
+    this.media.addEventListener('timeupdate', this._handleTimeUpdate);
+  }
 
-    constructor() {
+  _handleTimeUpdate() {
+    const currentTime = Math.floor(this.media.currentTime);
+    if (currentTime % 10 === 0) {
+      this._renderAd();
+    }
+  }
 
-        this.ads = Ads.getInstance();
-        this.adsContainer = document.createElement('div');
-        this._handleTimeUpdate = this._handleTimeUpdate.bind(this);
-
+  _renderAd() {
+    if (this.currentAd) {
+      return;
     }
 
-    run(player) {
-
-        this.player = player;
-        this.player.container.appendChild(this.adsContainer);
-        this.media = this.player.media;
-        this.media.addEventListener('timeupdate', this._handleTimeUpdate);
-
-    }
-
-    _handleTimeUpdate() {
-
-        const currentTime = Math.floor(this.media.currentTime);
-        if (currentTime % 10 === 0) {
-            this._renderAd();
-        }
-
-    }
-
-    _renderAd() {
-
-        if (this.currentAd) {
-            return;
-        }
-
-        const ad = this.ads.getAd();
-        this.currentAd = ad;
-        this.adsContainer.innerHTML = `
+    const ad = this.ads.getAd();
+    this.currentAd = ad;
+    this.adsContainer.innerHTML = `
       <div class="ads">
         <a class="ads__link" href="${this.currentAd.url}" target="_blank">
           <img class="ads__img" src="${this.currentAd.imageUrl}" />
@@ -49,12 +40,12 @@ class AdsPlugin {
       </div>
     `;
 
-        setTimeout(() => {
-            this.currentAd = null;
-            this.adsContainer.innerHTML = '';
-        }, 5000);
-    }
-
+    setTimeout(() => {
+      this.currentAd = null;
+      this.adsContainer.innerHTML = '';
+    }, 5000);
+  }
+  
 }
 
 export default AdsPlugin;
